3e79586b62d62f2503f54813b7db371a7990aa5c
kamal koushik duppalapudi <kamalkoushik24@gmail.com>
2020-08-21 14:36:50 +0530

    Completed the pokedex assignment

--- app/src/main/java/edu/harvard/cs50/pokedex/PokemonActivity.java
+++ app/src/main/java/edu/harvard/cs50/pokedex/PokemonActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -22,9 +26,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.jar.Attributes;
 
 public class PokemonActivity extends AppCompatActivity {
+    private class DownloadSpriteTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                return BitmapFactory.decodeStream(url.openStream());
+            } catch (IOException e) {
+                Log.e("cs50", "Download sprite error", e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
+            imageView.setContentDescription("Image of " + pokemonName);
+        }
+    }
     private TextView nameTextView;
     private TextView numberTextView;
     private TextView type1TextView;
@@ -35,6 +59,9 @@ public class PokemonActivity extends AppCompatActivity {
     private boolean caught = false;
     private String text;
     private String pokemonName;
+    private ImageView imageView;
+    private TextView descTextView;
+    private String descURL;
 
 
     @Override
@@ -49,6 +76,8 @@ public class PokemonActivity extends AppCompatActivity {
         type1TextView = findViewById(R.id.pokemon_type1);
         type2TextView = findViewById(R.id.pokemon_type2);
         crbutton = findViewById(R.id.crbutton);
+        descTextView = findViewById(R.id.pokemon_desc);
+        imageView = findViewById(R.id.image);
         text = crbutton.getText().toString();
         load();
 
@@ -66,6 +95,9 @@ public class PokemonActivity extends AppCompatActivity {
 
                     nameTextView.setText(response.getString("name"));
                     numberTextView.setText(String.format("#%03d", response.getInt("id")));
+                    String imageURL = response.getJSONObject("sprites").getString("front_default");
+                    new DownloadSpriteTask().execute(imageURL);
+
 
                     JSONArray typeEntries = response.getJSONArray("types");
                     for (int i = 0; i < typeEntries.length(); i++) {
@@ -83,6 +115,11 @@ public class PokemonActivity extends AppCompatActivity {
 
                     checkCatchStatus();
 
+                    int id = response.getInt("id");
+                    descURL = "https://pokeapi.co/pokemon-species/" + id;
+                    getDescription();
+
+
                 } catch (JSONException e) {
                     Log.e("cs50", "Pokemon json error", e);
                 }
@@ -94,6 +131,36 @@ public class PokemonActivity extends AppCompatActivity {
             }
         });
 
+        requestQueue.add(request);
+    }
+
+    private void getDescription() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, descURL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray descEntries = response.getJSONArray("flavor_text_entries");
+
+                    for (int i = 0; i < descEntries.length(); i++) {
+                        JSONObject entry = descEntries.getJSONObject(i);
+
+                        if (entry.getJSONObject("language").getString("name").equals("en")) {
+                            String description = entry.getString("flavor_text");
+                            descTextView.setText(description.replaceAll("\\r\\n|\\r|\\n", " "));
+                            break;
+                        }
+                    }
+                } catch (JSONException e) {
+                    Log.e("cs50", "pokemon description json error", e);
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("cs50", "pokemon description details error");
+            }
+        });
         requestQueue.add(request);
     }
 
